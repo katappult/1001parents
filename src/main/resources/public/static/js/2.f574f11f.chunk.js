3d3c6b8c7ec84d5511ea4943db5daa44206a7478
rmvonji <rmvonji@gmail.com>
2023-01-26 22:15:43 +0100

    first commit

--- src/main/resources/public/static/js/2.f574f11f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp1001parents=this.webpackJsonp1001parents||[]).push([[2],{1355:function(t,e,n){"use strict";n.r(e);var a=n(13),r=n(14),c=n(16),u=n(15),s=n(1),i=n.n(s),o=n(138),p=function(t){Object(c.a)(n,t);var e=Object(u.a)(n);function n(){return Object(a.a)(this,n),e.apply(this,arguments)}return Object(r.a)(n,[{key:"render",value:function(){return i.a.createElement(o.q,null)}}]),n}(s.Component);e.default=p}}]);
-//# sourceMappingURL=2.f574f11f.chunk.js.map
